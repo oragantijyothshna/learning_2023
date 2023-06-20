ac1e17e03d337ba29730aa2264153b66d4da8ba6
oraganti jyothshna <jyosthnaoraganti05@gmailcom>
2023-06-20 12:52:26 +0530

    Adding solutions

---
+++ solution1.1.c
@@ -0,0 +1,17 @@
+//1 if-else
+ #include <stdio.h>
+
+int find_biggest_ternary(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    int result = find_biggest_ternary(num1, num2);
+    printf("The biggest number is: %d\n", result);
+    return 0;
+}
+ 
+ 
